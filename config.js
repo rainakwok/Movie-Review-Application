1f6a636fda553cc8a96586ff1b9e9e5ce6771655
Raina Kwok <113138985+rainakwok@users.noreply.github.com>
2023-07-28 02:35:47 -0400

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-let config = {
-  host    : 'ec2-18-216-101-119.us-east-2.compute.amazonaws.com',
-  user    : 'r22kwok',
-  password: 'MSCI245',
-  database: 'r22kwok'
-};
- 
-export default config;
